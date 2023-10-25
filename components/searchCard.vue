3fe95a76071903c4eacce22d7f70466d868dc294
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 13:34:17 +0200

    Modified search bar I gave it rounded corners and added a search icon on the right side

--- components/searchCard.vue
+++ components/searchCard.vue
@@ -5,7 +5,10 @@
                 <h1 class="text-4xl pt-9 pb-7">Kurs og sertifisering</h1>
                 <p class="mb-1.5">Vi har en rekke kurs og sertifiseringer, <label class="font-bold underline underline-offset-2">se full liste her</label></p>
                 <p>Eller bruk søkefeltet under</p>
-                <input type="text" placeholder="Søk i kurs og sertifisering" class="search text-black font-extralight w-full py-2.5 px-10 mt-9 text-center placeholder:text-black placeholder:text-center">
+                <div class="relative">
+                    <input type="text" class="pl-16 w-full py-2.5 px-8 text-center rounded-sm placeholder-text-black text-center border" placeholder="Søk i kurs og sertifisering">
+                    <img src="~/public/images/searchIcon.png" alt="Search Icon" class="absolute right-4 top-1/2 transform -translate-y-1/2 w-6 h-6">
+                </div>
             </div>
         </div>
     </div>
@@ -20,7 +23,7 @@
         width: 1170px;
         height: 320px;
         margin: 0 auto;
-        background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('/public/images/img1.png');
+        background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('~/public/images/img1.png');
         background-size: cover;
     }
 </style>    
