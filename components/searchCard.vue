07b9776c86b2ec8fa3a05f511ca1a9f6217d4bcc
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 12:29:50 +0200

    Added background image Background image and component for "Kurs og sertifisering"

---
+++ components/searchCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="kurs-sertif-bg">
+        
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .kurs-sertif-bg {
+        width: 1170px;
+        height: 320px;
+        margin: 0 auto;
+        background-image: url('~/public/images/img1.png');
+        background-size: cover;
+    }
+</style>    
