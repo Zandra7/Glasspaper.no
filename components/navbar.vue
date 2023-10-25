d3f8ce8cfcad5028d6637228753e2b381f981d6b
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 21:12:58 +0200

    Added text and logo to navbar Rough sketch of how the navbar will look

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,17 @@
 <template>
     <nav class="navbar text-white">
-        <h1>Navbar</h1>
+        <div class="flex gap-2 justify-end">
+            <p>EN</p>
+            <p>|</p>
+            <p>NO</p>
+        </div>
+        <img src="~/public/images/logo.svg" alt="Glasspaper logo" class="w-64 pt-3">
+        <div class="pb-3 flex flex-row gap-12 justify-end">
+            <p>Tjenester</p>
+            <p>Om oss</p>
+            <p>Kontakt oss</p>
+            <p>Ledige stillinger</p>
+        </div>
     </nav>
 </template>
 
