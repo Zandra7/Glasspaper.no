aca7269069c692833b484773d46d14ba94eadf1e
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 12:57:01 +0200

    Added header, text and input

--- components/searchCard.vue
+++ components/searchCard.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
         <div class="kurs-sertif-bg">
-        
+            <div class="kurs-sertif-content mx-32 my-10 text-white">
+                <h1 class="text-4xl pt-9 pb-7">Kurs og sertifisering</h1>
+                <p class="mb-1.5">Vi har en rekke kurs og sertifiseringer, <label class="font-bold underline underline-offset-2">se full liste her</label></p>
+                <p>Eller bruk søkefeltet under</p>
+                <input type="text" placeholder="Søk i kurs og sertifisering" class="text-black font-extralight w-full py-2.5 px-10 mt-9 text-center placeholder:text-black placeholder:text-center">
+            </div>
         </div>
     </div>
 </template>
