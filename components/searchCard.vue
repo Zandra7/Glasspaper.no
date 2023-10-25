4124d8e8e8db4c084b5d782e5f4e1264d3eecff1
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 14:42:35 +0200

    Changes to margin, padding + added flex I fixed margin and padding so it is like it should be. I also put the content in a flex box to center it on the background img.

--- components/searchCard.vue
+++ components/searchCard.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
-        <div class="kurs-sertif-bg">
-            <div class="kurs-sertif-content mx-32 my-10 text-white">
+        <div class="kurs-sertif-bg flex flex-col">
+            <div class="kurs-sertif-content mx-32 text-white">
                 <h1 class="text-4xl pt-10 pb-7">Kurs og sertifisering</h1>
-                <p class="mb-2.5">Vi har en rekke kurs og sertifiseringer, <label class="font-bold underline underline-offset-2">se full liste her</label></p>
+                <p class="mb-2.5 text-2xl">Vi har en rekke kurs og sertifiseringer, <label class="font-bold underline underline-offset-2">se full liste her</label></p>
                 <p>Eller bruk søkefeltet under</p>
                 <div class="relative">
-                    <input type="text" class="pl-16 w-full py-2.5 px-8 text-center rounded-sm my-12 placeholder-text-black text-center border" placeholder="Søk i kurs og sertifisering">
+                    <input type="text" class="pl-16 w-full py-4 px-8 text-center rounded-sm my-12 placeholder:text-black placeholder:font-extralight placeholder:text-2xl text-center border" placeholder="Søk i kurs og sertifisering">
                     <img src="~/public/images/searchIcon.png" alt="Search Icon" class="absolute right-4 top-1/2 transform -translate-y-1/2 w-6 h-6">
                 </div>
             </div>
         </div>
-    </div>
 </template>
 
 <script setup>
@@ -25,5 +23,6 @@
         margin: 0 auto;
         background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('~/public/images/img1.png');
         background-size: cover;
+        margin-top: 2rem;
     }
 </style>    
