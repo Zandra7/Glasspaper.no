9c4e3bf1b1bec81bf2b409e09e7b8e3642611601
Sandra Elise Eng <sandra.eng@live.no>
2023-10-25 13:18:14 +0200

    Switched image and added linear gradient

--- components/searchCard.vue
+++ components/searchCard.vue
@@ -5,7 +5,7 @@
                 <h1 class="text-4xl pt-9 pb-7">Kurs og sertifisering</h1>
                 <p class="mb-1.5">Vi har en rekke kurs og sertifiseringer, <label class="font-bold underline underline-offset-2">se full liste her</label></p>
                 <p>Eller bruk søkefeltet under</p>
-                <input type="text" placeholder="Søk i kurs og sertifisering" class="text-black font-extralight w-full py-2.5 px-10 mt-9 text-center placeholder:text-black placeholder:text-center">
+                <input type="text" placeholder="Søk i kurs og sertifisering" class="search text-black font-extralight w-full py-2.5 px-10 mt-9 text-center placeholder:text-black placeholder:text-center">
             </div>
         </div>
     </div>
@@ -20,7 +20,7 @@
         width: 1170px;
         height: 320px;
         margin: 0 auto;
-        background-image: url('~/public/images/img1.png');
+        background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('/public/images/img1.png');
         background-size: cover;
     }
 </style>    
