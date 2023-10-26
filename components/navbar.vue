af90e3188d33d4936b61908813c33c6cdfa43bde
Sandra Elise Eng <sandra.eng@live.no>
2023-10-26 09:00:13 +0200

    Centered content of navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,16 +1,18 @@
 <template>
     <nav class="navbar text-white">
-        <div class="flex gap-2 justify-end">
-            <p>EN</p>
-            <p>|</p>
-            <p>NO</p>
-        </div>
-        <img src="~/public/images/logo.svg" alt="Glasspaper logo" class="w-64 pt-3">
-        <div class="pb-3 flex flex-row gap-12 justify-end">
-            <p>Tjenester</p>
-            <p>Om oss</p>
-            <p>Kontakt oss</p>
-            <p>Ledige stillinger</p>
+        <div class="content flex flex-col mx-auto">
+            <div class="flex gap-2 justify-end pt-3">
+                <p>EN</p>
+                <p>|</p>
+                <p>NO</p>
+            </div>
+            <img src="~/public/images/logo.svg" alt="Glasspaper logo" class="w-64">
+            <div class="pb-3 flex flex-row gap-12 justify-end">
+                <p>Tjenester</p>
+                <p>Om oss</p>
+                <p>Kontakt oss</p>
+                <p>Ledige stillinger</p>
+            </div>
         </div>
     </nav>
 </template>
@@ -22,5 +24,8 @@
 <style scoped>
     .navbar {
         background-color: rgba(88, 110, 114, 1);
+    }
+    .content {
+        max-width: 1170px;
     }
 </style>
