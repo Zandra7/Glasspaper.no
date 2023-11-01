911fe0b5f394dff9ab66bac7d0c4dd0afd1e992a
Sandra Elise Eng <sandra.eng@live.no>
2023-11-01 12:04:48 +0100

    Changed from tailwind to normal css

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,17 +1,17 @@
 <template>
     <nav class="navbar text-white">
-        <div class="content flex flex-col mx-auto">
-            <div class="flex gap-2 justify-end pt-3">
+        <div class="content">
+            <div class="lang">
                 <p>EN</p>
                 <p>|</p>
                 <p>NO</p>
             </div>
-            <img src="~/public/images/logo.svg" alt="Glasspaper logo" class="w-64">
-            <div class="pb-3 flex flex-row gap-12 justify-end">
-                <p class="cursor-pointer hover:underline hover:decoration-orange-500 hover:underline-offset-4">Tjenester</p>
-                <p class="cursor-pointer hover:underline hover:decoration-orange-500 hover:underline-offset-4">Om oss</p>
-                <p class="cursor-pointer hover:underline hover:decoration-orange-500 hover:underline-offset-4">Kontakt oss</p>
-                <p class="cursor-pointer hover:underline hover:decoration-orange-500 hover:underline-offset-4">Ledige stillinger</p>
+            <img src="~/public/images/logo.svg" alt="Glasspaper logo" class="gp-logo">
+            <div class="nav-items">
+                <p class="nav-item">Tjenester</p>
+                <p class="nav-item">Om oss</p>
+                <p class="nav-item">Kontakt oss</p>
+                <p class="nav-item">Ledige stillinger</p>
             </div>
         </div>
     </nav>
@@ -26,10 +26,38 @@
         background-color: rgba(88, 110, 114, 1);
     }
     .content {
+        display: flex;
+        flex-direction: column;
+        margin: 0 auto;
         max-width: 1170px;
         
         @media screen and (max-width: 1175px) {
             padding: 0 2rem;
         }
+    }
+    .lang {
+        display: flex;
+        flex-direction: row;
+        justify-content: end;
+        gap: 0.5rem;
+        padding-top: 1rem;
+    }
+    .gp-logo {
+        width: 16rem;
+    }
+    .nav-items {
+        display: flex;
+        flex-direction: row;
+        justify-content: end;
+        gap: 3rem;
+        padding-bottom: 1rem;
+    }
+    .nav-item {
+        cursor: pointer;
+    }
+    .nav-item:hover {
+        text-decoration: underline;
+        text-underline-offset: 4px;
+        text-decoration-color: #c44111;
     }
 </style>
