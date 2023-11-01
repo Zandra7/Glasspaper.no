e4753259dad5afced5ae5dc0c4eb1d2667affdf3
eugeniiee <eugenie.f@live.com>
2023-11-01 13:08:26 +0100

    Fixed flex properties

--- components/services.vue
+++ components/services.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div class="container">
-            <img src="~/public/images/Picture1.png" class="">
-            <img src="~/public/images/Picture2.png" class="flex-basis-1/4">
-            <img src="~/public/images/Picture3.png" class="flex-basis-1/4">
+            <img src="~/public/images/Picture1.png" class="pic1">
+            <img src="~/public/images/Picture2.png" class="pic2">
+            <img src="~/public/images/Picture3.png" class="pic3">
          </div>
     </div>
 </template>
@@ -17,16 +17,47 @@
 .container{
     display: flex;
     justify-content: center;
-    height: 200px
+    margin: auto;
+    gap: 39px;
+    margin-top: 39px;
+    max-width: 100%;
 }
 .pic1{
-
+    max-width: 460px;
+    height: 320px;
 }
 
 .pic2{
-    
+   max-width: 320px;
+   height: 320px; 
 }
 .pic3{
-
+    max-width: 320px;
+    height:320px;
 }
+
+@media(max-width: 1170px){
+    .container{
+    display: flex;
+    justify-content: center;
+    margin: auto;
+    gap: 39px;
+    margin-top: 39px;
+}
+.pic1{
+    max-width: 460px;
+    height: 320px;
+}
+
+.pic2{
+   max-width: 320px;
+   height: 320px; 
+}
+.pic3{
+    max-width: 340px;
+    height:320px;
+}
+}
+
+
 </style>
