81494c53a96f1dff96f58b1c85d8a8188abd0998
eugeniiee <eugenie.f@live.com>
2023-11-06 12:34:43 +0100

    The elements within container shrink with page

--- components/services.vue
+++ components/services.vue
@@ -1,66 +1,47 @@
 <template>
-  <div>
-    <div class="container">
-      <img src="~/public/images/Picture1.png" class="pic1">
-      <img src="~/public/images/Picture2.png" class="pic2">
-      <img src="~/public/images/Picture3.png" class="pic3">
+    <div>
+      <div class="container">
+        <img src="~/public/images/Picture1.png" class="pic1">
+        <img src="~/public/images/Picture2.png" class="pic2">
+        <img src="~/public/images/Picture3.png" class="pic3">
+      </div>
     </div>
-  </div>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  margin: auto;
-  gap: 39px; /* Reduced the gap for responsiveness */
-  margin-top: 20px; /* Reduced the margin for responsiveness */
-  max-width: 1170px; /* Make the container responsive */
-  width: 100%; /* Set a percentage-based width for the container */
-}
-
-.pic1 {
-  max-width: 460px;; /* Set a max-width for each image based on one-third of the container width */
-  height: auto; /* Maintain aspect ratio */
-}
-
-.pic2 {
-  width: 320px;
-  height: auto;
-}
-
-.pic3 {
-  max-width: 320px;
-  height: auto;
-}
-
-@media (max-width: 1170px) {
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    margin: auto;
+    gap: 39px; 
+    margin-top: 20px;
+    max-width: 1170px; 
+    width: 100%;
+  }
+  
+  .pic1 {
+    flex: 1;
+    max-width: 39.32%;
+    height: auto;
+  }
+  
+  .pic2 {
+    max-width: 27.35%;
+    height: auto;
+  }
+  
+  .pic3 {
+    max-width: 27.35%;
+    height: auto;
+  }
+  
+  @media (max-width: 1170px) {
     .container {
-  display: flex;
-  justify-content: center;
-  margin: auto;
-  gap: 39px; /* Reduced the gap for responsiveness */
-  margin-top: 20px; /* Reduced the margin for responsiveness */
-  max-width: 1170px; /* Make the container responsive */
-  width: 100%; /* Set a percentage-based width for the container */
-}
-
-.pic1 {
-  max-width: 460px;; /* Set a max-width for each image based on one-third of the container width */
-  height: auto; /* Maintain aspect ratio */
-}
-
-.pic2 {
-  width: 320px;
-  height: auto;
-}
-
-.pic3 {
-  max-width: 320px;
-  height: auto;
-}
-}
-</style>
+      gap: 10px;
+      margin-top: 10px;
+    }
+  }
+  </style>
