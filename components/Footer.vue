2ab256f3e7d73e97870ba601534f4cd5a680c0e0
eugeniiee <eugenie.f@live.com>
2023-11-08 13:38:44 +0100

    created grid container with classes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="background">
-        <h1>hello</h1>
+        <div class="container">
+            <div class="a">Glasspaper deler</div>
+            <div class="b">Konsernledelse</div>
+            <div class="c">Privacy and cookie</div>
+            <div class="c">Ofte stilte spørsmål</div>
+            <div class="e">Våre lokasjoner</div>
+            <div class="f">Legal Terms of Service</div>
+            <div class="g">Våre selskaper</div>
+            <div class="h">ikoner</div>
+            <div class="i">Copyright og Trademark</div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +20,40 @@
 
 <style scoped>
 .background{
-    background-color: rgb(67, 67, 67)
+    background-color: rgba(67, 67, 67,1)
 }
+
+.container{
+    color: white;
+    display:grid;
+    grid-template-areas:
+    "a b c"
+    "d e f"
+    "g h i";
+    padding-top: 65px;
+    align-items: center;
+}
+
+.a, .d, .g{
+    font-weight: 700px;
+}
+
+.d, .e, .f{
+    font-weight: 700px;
+}
+
+.g, .h, .i{
+    font-weight: 400px
+}
+
+@media(max-width:1000px){
+    .container{
+    color:brown;
+    display:grid;
+    grid-template-rows: auto auto auto auto auto auto auto auto auto;
+    grid-auto-flow:column;
+    font-size: 18px;
+}
+}
+
 </style>
