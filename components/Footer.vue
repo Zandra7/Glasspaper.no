c711299d8807ac214c24d885cf17bca6e5b7ecca
eugeniiee <eugenie.f@live.com>
2023-11-08 10:49:54 +0100

    created background color

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="background">
-        hello
+
     </div>
 </template>
 
@@ -9,9 +9,7 @@
 </script>
 
 <style scoped>
-
 .background{
-    background-color: rgba(67, 67, 67, 1)
+    background-color: rgba(67, 67, 67,)
 }
-
 </style>
